3ec39e9bf87f981ed2ea0c9d7af2d9c6d2c03a55
Christian Thorvik <christian96-th@hotmail.com>
2023-03-01 22:39:50 +0100

    Initial commit

---
+++ GuitarApp/src/ui/Fretboard/Fretboard.tsx
@@ -0,0 +1,13 @@
+﻿export const FretBoard = (props) => {
+  return (
+    <div
+      className="fretboard">
+      <p>Fucking
+         fuck
+         you
+         you
+         fucking
+         motherfucking
+         fuck</p>
+    </div>);
+};
